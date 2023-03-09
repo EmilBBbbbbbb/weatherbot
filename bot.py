8b7801f582ca35aaf2797da9d1edbf3c790c4ca4
Emiian <emilianevgenevishborisov@gmail.com>
2023-03-10 00:33:27 +0300

    +bot 2.0

--- bot.py
+++ bot.py
@@ -4,41 +4,97 @@ import requests
 from config import week
 from config import open_wether_token
 from  datetime import  datetime
+from telebot import types
 
 bot=telebot.TeleBot(bot_token)
 
+city=None
+chat_id=None
+
+chch=False
 def data(plus):
-    a = datetime.weekday( datetime.now())+plus
-    if a>6:
+    a = datetime.weekday(datetime.now()) + plus
+    if a > 6:
         a-=6
     return week[a]
+
 
 def wether_day(data, day, chat_id):
     dt=data["list"][8*day]["dt_txt"][:10]
     temp=data["list"][8*day]["main"]["temp"]
     wind=data["list"][8*day]["wind"]["speed"]
+
+    marckup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item0 = types.KeyboardButton("Сегодня")
+    item1 = types.KeyboardButton("Завтра")
+    item2 = types.KeyboardButton("5 дней")
+
+    marckup.add(item0, item1, item2)
+
     bot.send_message(chat_id, f"Погода на {dt}:\n"
               f"Температура: {temp}°С\n"
-              f"Скорость ветра: {wind}м/с\n")
-@bot.message_handler(commands=["start"])
+              f"Скорость ветра: {wind}м/с\n", reply_markup=marckup)
+
+
+
+@bot.message_handler(commands=["start","city"])
 def start(message):
     bot.send_message(message.chat.id, "Введите город:")
 
+    return True
 
-@bot.message_handler()
+
+@bot.message_handler(content_types=["text"])
 def get_wether(message):
-    try:
-        r= requests.get(
-            f"https://api.openweathermap.org/data/2.5/forecast?q={message.text}&appid={open_wether_token}&units=metric"
-        )
-        data= r.json()
-        chat_id = message.chat.id
+    global city
+    global chat_id
+    chat_id = message.chat.id
+    if message.text == "Завтра":
+        try:
+            r = requests.get(
+                f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_wether_token}&units=metric"
+            )
+            data = r.json()
+            wether_day(data, 1, chat_id)
+        except Exception as ex:
+            print(ex)
 
-        wether_day(data, 1,chat_id)
+    elif message.text == "5 дней":
+        try:
+            r = requests.get(
+                f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_wether_token}&units=metric"
+            )
+            data = r.json()
+
+            wether_day(data, 0, chat_id)
+            wether_day(data, 1, chat_id)
+            wether_day(data, 2, chat_id)
+            wether_day(data, 3, chat_id)
+            wether_day(data, 4, chat_id)
+        except Exception as ex:
+            print(ex)
+
+    elif message.text == "Сегодня":
+        try:
+            r = requests.get(
+                f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_wether_token}&units=metric"
+            )
+            data = r.json()
+            wether_day(data, 0, chat_id)
+        except Exception as ex:
+            print(ex)
 
 
+    else:
+        city = message.text
+        try:
+            r = requests.get(
+                f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_wether_token}&units=metric"
+            )
+            data = r.json()
+            wether_day(data, 0, chat_id)
+        except Exception as ex:
+            print(ex)
 
-    except Exception as ex:
-        bot.send_message(message.chat.id,"Проверьте название города")
 
 bot.polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 open_wether_token="399acdcd22e64340ff0da3cd6b5c71dd"
 bot_token="6109938210:AAFHm6Dof4gYj0tX1iEGZBpmzAyBOPheWX4"
 week=["Пн","Вт","Ср","Чт","Пт","Сб","Вс"]
+
+
+
