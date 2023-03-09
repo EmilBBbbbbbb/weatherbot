e81aa26564695eba78bb5aac7ce7e2d4005743ec
Emiian <emilianevgenevishborisov@gmail.com>
2023-03-09 20:11:06 +0300

    weather2.0+bot1.1

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import telebot
 from config import bot_token
 import requests
-from pprint import pprint
+from config import week
 from config import open_wether_token
+from  datetime import  datetime
 
 bot=telebot.TeleBot(bot_token)
 
+def data(plus):
+    a = datetime.weekday( datetime.now())+plus
+    if a>6:
+        a-=6
+    return week[a]
 
+def wether_day(data, day, chat_id):
+    dt=data["list"][8*day]["dt_txt"][:10]
+    temp=data["list"][8*day]["main"]["temp"]
+    wind=data["list"][8*day]["wind"]["speed"]
+    bot.send_message(chat_id, f"Погода на {dt}:\n"
+              f"Температура: {temp}°С\n"
+              f"Скорость ветра: {wind}м/с\n")
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "Введите город:")
@@ -16,22 +29,15 @@ def start(message):
 def get_wether(message):
     try:
         r= requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_wether_token}&units=metric"
+            f"https://api.openweathermap.org/data/2.5/forecast?q={message.text}&appid={open_wether_token}&units=metric"
         )
         data= r.json()
+        chat_id = message.chat.id
 
-        #pprint(data) #all wether
-
-        city=message.text
-        temp_now=data["main"]["temp"]
-        wind = data["wind"]["speed"]
-        pressure = data["main"]["pressure"]
+        wether_day(data, 1,chat_id)
 
 
-        bot.send_message(message.chat.id, f"Погода в городе {city} на сегодня:\n"
-              f"Температура: {temp_now}°С\n"
-              f"Скорость ветра: {wind}м/с\n"
-              f"Давление: {pressure}мм р.с\n")
+
     except Exception as ex:
         bot.send_message(message.chat.id,"Проверьте название города")
 
